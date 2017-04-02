aa7bd415c92c31305cb97c246f6806631333e2c2
dhilipsiva <dhilipsiva@gmail.com>
2017-04-02 16:50:18 +0530

    Implement `mul` function

--- math.py
+++ math.py
@@ -1,4 +1,14 @@
 def sum(a,b):
+    """
+    Its always recommended to write documentation of your function here
+    delete these two comments and write your doc an push it
+    """
     c = a+b
     print(c)
 
+
+def mul(a, b):
+    """
+    This function multiplies 2 functions and returns the value
+    """
+    return a * b
