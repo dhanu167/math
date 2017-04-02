44cac291a3c4caceeaa8c3dd2143a592f80b0884
dhilipsiva <dhilipsiva@gmail.com>
2017-04-02 21:40:48 +0530

    Some formatting chages

--- math.py
+++ math.py
@@ -1,9 +1,8 @@
-def sum(a,b):
+def sum(a, b):
     """
     This function adds two numbers and returns the value
     """
     return a+b
-   
 
 
 def mul(a, b):
@@ -11,9 +10,10 @@ def mul(a, b):
     This function multiplies 2 functions and returns the value
     """
     return a * b
-	
-def div(a,b):
+
+
+def div(a, b):
     """
-	this function div 2 nos
-	"""
-	return a / b
+    this function div 2 nos
+    """
+    return a / b
